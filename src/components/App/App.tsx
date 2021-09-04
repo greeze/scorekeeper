4e57d5350e7f9920df23fe3957cbf4f80b8235e1
Joe Gores <joegores@invisionapp.com>
2021-09-04 15:45:27 -0700

    Tear out some old stuff. Start experimenting.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import SimpleKeeper from 'components/SimpleKeeper'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto text-center'>
+      <h1 className=''>Here's an H1</h1>
+      <SimpleKeeper />
     </div>
   )
 }
---
+++ src/components/SimpleKeeper/SimpleKeeper.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SimpleKeeper() {
+  return <div>Here's some text inside the SimpleKeeper component, yo.</div>
+}
+
+export default SimpleKeeper
