96bd6d3308610fc9522a0d89f5a87143fd5bf880
Joe Gores <joegores@invisionapp.com>
2021-09-04 14:18:02 -0700

    Directory structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { RecoilRoot } from 'recoil'
 import './index.css'
-import App from './App'
+import App from 'components/App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
