db81e3122ca208e5f5ec5bb0753c12645deb3379
Joe Gores <joegores@invisionapp.com>
2021-09-05 01:55:08 -0700

    Use regexes to prevent style purge

--- config/.tailwindrc.js
+++ config/.tailwindrc.js
@@ -1,25 +1,9 @@
-const screenSizes = ['', 'sm:', 'md:', 'lg:', 'xl:']
-const colClasses = []
-for (let i = 1; i <= 12; i++) {
-  screenSizes.forEach((size) => {
-    colClasses.push(`${size}grid-cols-${i}`)
-  })
-}
-
 module.exports = {
   purge: {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    safelist: [
-      ...colClasses,
-      'text-gray-500',
-      'text-red-500',
-      'text-yellow-500',
-      'text-green-500',
-      'text-blue-500',
-      'text-indigo-500',
-      'text-purple-500',
-      'text-pink-500',
-    ],
+    safelist: {
+      standard: [/^(\w\w:)?text-.+-\d+$/, /^(\w\w:)?grid-cols-\d+$/],
+    },
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
